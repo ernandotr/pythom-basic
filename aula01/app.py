d76a042c63cc2dbe611c6e4613023cf542cccf57
Ernando Rezende <ernando.03@gmail.com>
2024-12-06 23:39:57 +0000

    First commit

---
+++ aula01/app.py
@@ -0,0 +1,34 @@
+# String
+nome = "Ernando"
+print("Nome:", nome)
+
+#Integer
+idade = 18
+print ("Idade:", idade)
+
+# Float
+altura = 1.81
+print("Altura:", altura)
+
+#Boolean
+situacao = True
+print("Situação:", situacao)
+
+# List
+numeros =   [2, 3, 5, 7, 11]
+print("Números:", numeros)
+print("Número:", numeros[2])
+
+# Dictionaries
+usuario = {
+    "nome": "Ernando",
+    "idade": 18,
+    "cidade": "Oeiras"
+
+}
+print(usuario)
+print("Cidade:", usuario["cidade"])
+
+#Tuples (immutable)
+curso = ("Curso de Python e Django", "Cesar", 2024, 197.52)
+print("Título:", curso[0])
---
+++ aula02/app.py
@@ -0,0 +1,84 @@
+# Operadores aritmeticos
+
+a = 5
+b = 3
+
+# Soma
+print("Soma: ", a + b)
+
+# Subtração 
+print("Subtração: ", a - b)
+
+# Multiplicação 
+print("Multiplicação: ", a * b)
+
+# Divisão
+print("Divisão: ", a / b)
+
+# Divisão inteira
+print("Divisão Inteira; ", a // b)
+
+# Módulo / Resto da divisão
+
+print("Módulo: ", a % b)
+
+# Exponenciação 
+print("Exponenciação: ", a ** b)
+
+# Operadores de atribuição
+# Atribuição simples
+x = 5
+
+# Atrbuiçã0 com adição
+x += 5
+print("Atrbuiçã0 com adição: ", x)
+
+# Atrbuiçã0 com Subtração
+x -= 5
+print("Atrbuiçã0 com subtração: ", x)
+
+# Atrbuiçã0 com Multiplicação
+x *= 5
+print("Atrbuiçã0 com Multiplicação: ", x)
+
+# Atrbuiçã0 com divisão
+x /= 5
+print("Atrbuiçã0 com divisão: ", x)
+
+
+# Operadores de comparação
+
+a = 10
+b = 5
+
+# Igual
+print("Igual ", a == b)
+
+# Diferente
+print("Diferente: ", a != b)
+
+# Maior que
+print("Maior que: ", a > b)
+
+# Menor que
+print("Menor que: ", a < b)
+
+# Maior ou igual 
+print("Maior ou igual: ", a >= b)
+
+# Menor ou igual
+print("Menor ou igual: ", a <= b)
+
+# Operadores lógicos
+a = True
+b = True
+
+# Operador AND
+print("AND:", a and b)
+
+# Operador OR
+print("OR:", a or b)
+
+#Operador NOT
+print("NOT:", not a)
+
