73cbfcc19639208d844b32e14a8abee9a98311d2
Ernando Rezende <ernando.03@gmail.com>
2025-05-29 22:54:56 +0100

    Seting up a English voice to speak a french text

--- text-to-audio.py
+++ text-to-audio.py
@@ -24,6 +24,33 @@ if __name__ == "__main__":
     # change_voice(engine, "nl_BE", "VoiceGenderFemale")
     # engine.say("Hello World")
     # engine.runAndWait()
+    # Set the language Portuguese (Brazil) and the first voice available
+    # change_voice(engine, lenguage, "VoiceGenderNeuter")  # Change to Portuguese (Brazil) neuter voice
+    engine.setProperty('voice', "com.apple.eloquence.pt-BR.Sandy")  # Set to Portuguese (Brazil) voice
+    speak(engine, "Olá, este é um teste de conversão de texto em fala.")
+    speak(engine, "Como posso ajudá-lo hoje?")
+    speak(engine, "Adeus!")
+    speak(engine, "Este é um exemplo simples de conversão de texto em fala com Python.")
+    speak(engine, "Obrigado por usar este programa.")
+    speak(engine, "Tenha um ótimo dia!")
+    speak(engine, "Este é um exemplo de biblioteca pyttsx3.")
+    speak(engine, "Sinta-se à vontade para modificar o texto conforme necessário.")
+    speak(engine, "Este código foi projetado para converter texto em fala.")
+    speak(engine, "Você pode usá-lo para várias aplicações.")
+
+    # Set the language English (United States) and the first voice available
+    # change_voice(engine, lenguage2, "VoiceGenderNeuter")  # Change to English (United States) neuter voice
+    engine.setProperty('voice', "com.apple.voice.compact.en-US.Samantha")  # Set to English (United States) voice
+    speak(engine, "Hello, this is a text-to-speech conversion test.")
+    speak(engine, "How can I assist you today?")
+    speak(engine, "Goodbye!")
+    speak(engine, "This is a simple example of text-to-speech conversion with Python.")
+    speak(engine, "Thank you for using this program.")
+    speak(engine, "Have a great day!")
+    speak(engine, "This is a demonstration of the pyttsx3 library.")
+    speak(engine, "Feel free to modify the text as needed.")
+    speak(engine, "This code is designed to convert text to speech.")
+    speak(engine, "You can use it for various applications.")
 
     
     #Set the language French (France) and the first voice available
