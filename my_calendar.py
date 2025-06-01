271c7baca074abf8f52ab0b4691f5fc6d631d56e
Ernando Rezende <ernando.03@gmail.com>
2025-06-01 15:38:10 +0100

    Display the calendar for the entire year

--- my_calendar.py
+++ my_calendar.py
@@ -1,5 +1,5 @@
 import calendar
 
-year = int(input('Enter Year:'))
+year = int(input('Enter Year: '))
 
-print(calendar.calendar(year, 3, 1, 8, 3))
+print(calendar.calendar(year, 3, 1, 8)) # Display the calendar for the entire year with a width of 3, 1 month per line, and 8 lines per page
