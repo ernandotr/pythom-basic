809b665bd4134f0a70a261bedc2a5bcd2bcc468b
Ernando Rezende <ernando.03@gmail.com>
2025-06-01 15:45:38 +0100

    Count the number of leap days in a given period

--- leapYear.py
+++ leapYear.py
@@ -6,3 +6,4 @@ userYear = input('Enter Year: ')  # Prompt user for a year
 year = int(userYear) if userYear else current_year  # Use current year if no input is given
 
 print(calendar.isleap(year)) # Check if the year is a leap year
+print(calendar.leapdays(2000, 2024)) # Count the number of leap days between 2000 and 2024`
