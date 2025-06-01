aaba5e593decb0dc26fb7318b2eac624ee910dd0
Ernando Rezende <ernando.03@gmail.com>
2025-06-01 15:40:11 +0100

    print the abbreviated name of the manth

--- my_calendar.py
+++ my_calendar.py
@@ -3,5 +3,5 @@ import calendar
 year = int(input('Enter Year: '))
 month = int(input('Enter Month: '))
 print(calendar.month_name[month]) # Get the full name of the month
-
+print(calendar.month_abbr[month]) # Get the abbreviated name of the month
 print(calendar.calendar(year, 3, 1, 8)) # Display the calendar for the entire year with a width of 3, 1 month per line, and 8 lines per page
