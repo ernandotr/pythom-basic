ed315286ef3da456d6b733e1c03b9e8a611a612d
Ernando Rezende <ernando.03@gmail.com>
2025-06-01 15:34:57 +0100

    Print the weekday of the first day of month

--- week_days.py
+++ week_days.py
@@ -5,3 +5,4 @@ month = int(input('Enter Month: '))  # Prompt user for a month
 print(calendar.month(year, month))  # Display the month's calendar
 
 print(calendar.monthrange(year, month)) # Get the first weekday and number of days in the month
+print(calendar.weekday(year, month, 1)) # Get the weekday of the first day of the month
