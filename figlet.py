9c062af2f839dd2fe7d68b7414fac6e8bdaca180
Ernando Rezende <ernando.03@gmail.com>
2025-06-19 10:20:58 +0100

    Added color to output

--- figlet.py
+++ figlet.py
@@ -1,7 +1,10 @@
 import pyfiglet
+from colorama import Fore
 
 def print_figlet(text):
-    f = pyfiglet.Figlet(font='slant')
-    print(f.renderText(text))
+    f = pyfiglet.figlet_format(text, font="slant")
+    print(Fore.GREEN+f)
 
 print_figlet("PyFiglet")
+print_figlet("Hello, World!")
+print_figlet("This is a test of PyFiglet.")
