2e02e1b87ededa500019b2a0a2f6b7aee4f786c9
Ernando Rezende <ernando.03@gmail.com>
2025-06-23 23:49:34 +0100

    App to print a colorfull calendar

---
+++ colorfull_calendar.py
@@ -0,0 +1,33 @@
+import calendar
+from rich.console import Console
+from rich.table import Table
+
+def display_colored_calendar(year, month):
+    console = Console()
+    months = [calendar.monthcalendar(year, m) for m in range(1, 13)]
+
+    WEEK_DAYS_COLOR = "bold blue"
+    month_name = calendar.month_name[month]
+    print(months)
+    # Create a table for the month
+    table = Table(title= f"[bold cyan]{month_name} {year}[/bold cyan]", show_lines=True)
+    
+    # Add the days of the week as headers
+    table.add_column("Sun", justify="center", style="bold red")
+    table.add_column("Mon", justify="center", style=WEEK_DAYS_COLOR)
+    table.add_column("Tue", justify="center", style=WEEK_DAYS_COLOR)
+    table.add_column("Wed", justify="center", style=WEEK_DAYS_COLOR)
+    table.add_column("Thu", justify="center", style=WEEK_DAYS_COLOR)
+    table.add_column("Fri", justify="center", style=WEEK_DAYS_COLOR)
+    table.add_column("Sat", justify="center", style="bold white")
+
+    # Fill in the days of the month
+    for week in months[month]:
+        table.add_row(*[(str(day) if day != 0 else "") for day in week])    
+
+    # Print the colored calendar
+    console.print(table)
+    console.print("\n")
+
+if __name__ == "__main__":
+    display_colored_calendar(2025, 12)
