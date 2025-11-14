63a8caf2b0f677c3820ee53354e5b50a9cbd3a6d
ernandorezende-arch <ernandorezende@frwk.com.br>
2025-11-14 00:04:22 +0000

    Implemented a lightning flash

---
+++ LightningFlash.py
@@ -0,0 +1,8 @@
+import time, os, random
+
+for _ in range(12):
+    # os.system('cls' if os.name == 'nt' else 'clear')
+    flash = " " * random.randint(0,30) + " ⚡ "
+    print(flash)
+    time.sleep(0.15)
+# os.system('cls' if os.name == 'nt' else 'clear')
